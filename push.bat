2b9b193f14b47308ec8a648c16c9daaaf73cd997
lotos <lotositsh@gmail.com>
2026-06-14 12:39:42 +0300

    here 14.06.2026 12:39

--- push.bat
+++ push.bat
@@ -2,11 +2,10 @@
 timeout /t 2 /nobreak > nul
 setlocal enabledelayedexpansion
 
-rem Получаем текущую дату и время
+
 for /f "tokens=1-3 delims= " %%a in ('date /t') do set currentDate=%%a
 for /f "tokens=1-2 delims=:" %%a in ('time /t') do set currentTime=%%a:%%b
 
-rem Формируем сообщение коммита
 set commitMessage=here !currentDate! !currentTime!
 
 git status
